eae4fb3f8e7bec913cab8585db141d4ba1a56e0e
Abdulazeez403 <Abdulazeezsodiq403@gmail.com>
2022-06-23 10:38:34 -0700

    tailwind configured

--- src/App.js
+++ src/App.js
@@ -1,52 +1,11 @@
-import "./App.css";
-import { AiFillYoutube } from "react-icons/ai";
-import { BsTwitter } from "react-icons/bs";
-import { IoMdPaperPlane } from "react-icons/io";
-import Logo from "./img/cleverLogo .png";
+import HomePage from "./components/Home";
 
-function App() {
-  // const setThemePreference = () => {
-  let d = new Date();
-
-  let currentHour = d.getHours();
-
-  // if (currentHour >= 19 || currentHour <= 6) {
-  //   document.body.setAttribute("data-theme", "dark_theme");
-  // } else {
-  //   document.body.setAttribute("data-theme", "light_theme");
-  // }
-  // };
-
-  // window.onload = setThemePreference;
-
-  return (
-    <div
-      className={
-        currentHour >= 19 || currentHour <= 6
-          ? ("data-theme", "dark_theme")
-          : ("data-theme", "light_theme")
-      }
-    >
-      <img src={Logo} alt="logo" />
-      <div className="wrapper">
-        <h1>
-          Coming soon<span className="dot">.</span>
-        </h1>
-        <p>Our website is under construction.</p>
-        <div className="icons">
-          <a href="./">
-            <BsTwitter />
-          </a>
-          <a href="./">
-            <AiFillYoutube />
-          </a>
-          <a href="./">
-            <IoMdPaperPlane />
-          </a>
-        </div>
-      </div>
-    </div>
-  );
+const App = () => {
+ return (  
+  <>
+  <HomePage/>
+  </>
+ );
 }
-
+ 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,24 @@
+
+const HomePage = () => {
+ return ( 
+
+  <div  className="container mx-auto bg-green ">
+   <div className="inline-flex justify-center  px-4">
+
+   <div className=" mt-10">
+    <h3 className="font-bold text-7x1">It's too late, you can still have a high pay tech skill. </h3 >
+    <p className="bg-green-2000" >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non praesentium eligendi reprehenderit facilis vitae odit ipsum fugiat eos id aperiam?</p>
+   </div>
+
+   <div className="">
+    <img src="./img/imageLogo .png" className="w-50" 
+    style={{width: 1500}} alt="logo" />
+   </div>
+
+  </div>
+  </div>
+
+  );
+}
+ 
+export default HomePage;
