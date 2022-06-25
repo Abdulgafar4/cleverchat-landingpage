cc46815753bd0e970752ab6ebc9e293a76e9feff
Abdulazeez403 <Abdulazeezsodiq403@gmail.com>
2022-06-25 09:32:17 -0700

    fixed bugs

--- src/components/introducting.js
+++ src/components/introducting.js
@@ -2,8 +2,8 @@ const IntroducingPage = () => {
  return ( 
   <div className="container">
    <div>
-    <di>
-     </di v>
+    <div>
+     </div>
      <div>
       <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h4>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, est?</p>
